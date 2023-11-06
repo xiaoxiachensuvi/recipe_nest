7ced63c6edf525bd1e2376afccf195161afadf28
Xiaoxia Chen <xiaoxia.chen@comptel.com>
2023-11-06 20:01:53 +0200

    Handle transcript -> recipe
    add some example.json for mock data, as the open ai costs and takes time to generate

--- src/UploadRecipe.jsx
+++ src/UploadRecipe.jsx
@@ -1,59 +1,88 @@
-import {useRef, useEffect, useState} from 'react';
-import axios from "axios";
+import { useRef, useEffect, useState } from 'react';
+import axios from 'axios';
 
-const VITE_OPENAI_API_KEY= import.meta.env.VITE_OPENAI_API_KEY;
-const model = "whisper-1";
-
+const VITE_OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 function UploadRecipe() {
   const inputRef = useRef();
   const [file, setFile] = useState();
   const [response, setResponse] = useState(null);
+  const [recipe, setRecipe] = useState('');
 
   const onChangeFile = () => {
     setFile(inputRef.current.files[0]);
   };
 
-  useEffect(() =>{
+  useEffect(() => {
     const fetchAudioFile = async () => {
-      if(!file){
+      if (!file) {
         return;
       }
 
       const formData = new FormData();
-      formData.append("model", model);
-      formData.append("file", file);
+      formData.append('file', file);
 
-
-      axios
-        .post("https://api.openai.com/v1/audio/transcriptions", formData, {
-          headers: {
-            "Content-type": "multipart/form-data",
-            Authorization: `Bearer ${VITE_OPENAI_API_KEY}`
+      // https://api.openai.com/v1/audio/transcriptions
+      //
+      try {
+        const transcriptionResponse = await axios.post(
+          './transcription_example.json',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              Authorization: `Bearer ${VITE_OPENAI_API_KEY}`,
+            },
           }
-        })
-        .then((res) => {
-            console.log(res.data);
-            setResponse(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+        );
+
+        setResponse(transcriptionResponse.data);
+
+        // https://api.openai.com/v1/completions
+        const summaryResponse = await axios.post(
+          './recipe_example.json',
+          {
+            model: 'gpt-3.5-turbo-instruct',
+            prompt: `Please summarise the text field as a recipe. The recipe should have a title of recipe name and two sections. the first section list down a table of all ingredients and the amount in a table, this section is titled with "Ingredient" wrapped in HTML tag <H2>. If the text contains a sequence of instructions, create a second section list down the steps, this section is title with "Steps" wrapped in HTML tag <H2>. The HTML is wrapped in <div class="recipe">, assuming it'll be put directly under <body> tag. Text = ${JSON.stringify(transcriptionResponse.data, null, 2)}`,
+            max_tokens: 1024,
+            temperature: 0,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${VITE_OPENAI_API_KEY}`,
+            },
+          }
+        );
+        console.log('response', summaryResponse);
+
+        if (summaryResponse.data.choices && summaryResponse.data.choices.length > 0 && summaryResponse.data.choices[0].text) {
+          const recipeText = summaryResponse.data.choices[0].text;
+          setRecipe(recipeText);
+        } else {
+          console.error('No text found in summary response.');
+        }
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     fetchAudioFile();
-  },[file]);
+  }, [file]);
 
   return (
     <div>
       <input
         ref={inputRef}
-        type="file"
-        accept=".mp3, .m4a"
+        type='file'
+        accept='.mp3, .m4a'
         onChange={onChangeFile}
-        style={{ display: "block", marginTop: "20"}}
+        style={{ display: 'block', marginTop: '20px' }}
       />
-    {response && <div>{JSON.stringify(response, null, 2)}</div>}
+      {recipe && <div dangerouslySetInnerHTML={{ __html: recipe }} />}
+
+      {response && <div>{JSON.stringify(response, null, 2)}</div>}
+
     </div>
   );
 }
